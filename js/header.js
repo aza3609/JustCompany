e8acb415eb50589c5fe7c6ab883ab2c20ed292aa
aza3609 <89471930+aza3609@users.noreply.github.com>
2024-08-05 06:35:02 +0900

    update

---
+++ js/header.js
@@ -0,0 +1,84 @@
+const header = document.querySelector("header");
+const headerLogo = document.getElementById("header-logo__img");
+
+function headerDropdown() {
+    header.classList.add("over");
+    header.style.color = "black"
+    header.style.backgroundColor = "rgba(243, 243, 243)"
+    headerLogo.src = "src/justcompany-logo.svg";
+    header.style.height = "245px"
+    header.style.transition = "height 0.7s ease-in-out"
+}
+
+function headerDropup() {
+  header.classList.remove("over");
+  headerLogo.src = "src/justcompany-logo_white.svg";
+  header.style.height = "100px"
+  header.style.transition = "height 0s ease-in-out"
+  
+  let nowScrollTop = window.scrollY;
+  if (nowScrollTop < 10){
+    header.style.color = "white"
+    header.style.backgroundColor = "rgba(243, 243, 243,00)"
+  }
+  else {
+    headerLogo.src = "src/justcompany-logo.svg";
+  }
+}
+
+let prevScrollTop = 0;
+window.addEventListener("scroll", function() {
+  let nowScrollTop = window.scrollY;
+  if (nowScrollTop > prevScrollTop){
+    //스크롤 내림
+        header.style.transform = "translateY(-100px)"
+        console.log('Down!');
+        header.style.height = "100px"
+        header.style.transition = "height 0s ease-in-out"
+        header.style.transition = "transform 0.7s ease-in-out"
+  }
+  else if(nowScrollTop < prevScrollTop){
+    //스크롤 올림
+    header.style.transform = "translateY(0px)"
+    header.style.backgroundColor = "rgba(243, 243, 243)"
+    header.style.color = "black"
+    console.log('Up!');
+    headerLogo.src = "src/justcompany-logo.svg";
+    header.style.height = "100px"
+    header.style.transition = "height 0s ease-in-out"
+    header.style.transition = "transform 0.7s ease-in-out"
+  }
+    prevScrollTop = nowScrollTop;
+
+    if (nowScrollTop < 10){
+      header.style.color = "white"
+      header.style.backgroundColor = "rgba(243, 243, 243,00)"
+      headerLogo.src = "src/justcompany-logo_white.svg";
+    }
+  });
+
+header.addEventListener("mouseover", headerDropdown);
+header.addEventListener("mouseleave", headerDropup);
+
+
+
+
+
+/* function headerToggle() {
+  header.classList.toggle("active");
+  header.querySelector(".header__logo img").classList.toggle("active");
+}
+
+function handleResize() {
+  const width = window.innerWidth;
+  if (header.classList.contains("active")) {
+    if (width > 900) {
+      header.classList.remove("active");
+    } else {
+      header.classList.add("active");
+    }
+  }
+} */
+
+/* hamburgerBtn.addEventListener("click", headerToggle);
+window.addEventListener("resize", handleResize); */
